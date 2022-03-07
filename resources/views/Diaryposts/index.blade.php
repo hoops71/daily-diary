b020e9092c9f4d6b5f40d54982612d41d351e012
hoops71 <martin@mahooper.com>
2022-03-07 20:12:11 +0100

    Added modal to delete

--- resources/views/Diaryposts/index.blade.php
+++ resources/views/Diaryposts/index.blade.php
@@ -11,11 +11,11 @@
                     <p>{{ $apost->content }}</p>
                     <p>Last edited: {{ $apost->updated_at->diffForHumans() }}</p>
                     <a href="{{ route('diary.show', $apost->id) }}" id="show" class="btn btn-primary btn-sm">View</a>
-                    <a class="btn btn-danger btn-sm" href="#" id="delete" onclick="event.preventDefault();
+                    <a class="btn btn-danger btn-sm" href="#" id="delete" onclick="if(!confirm('Are you sure you want to delete the post?')) return event.preventDefault();
                      document.getElementById(
                           'delete-form-{{ $apost->id }}').submit();" >
                           Delete
-                        </a>
+                    </a>
                     <form id="delete-form-{{ $apost->id}}"  action="{{ route('diary.destroy',  $apost->id) }}" method="POST" >
                         @csrf
                         @method('DELETE')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <div class="mt-4 p-5 bg-primary text-white rounded ">
         <h1 class="text-center">Welcome to My Diary</h1>
         @if (Auth::guest())
-          <p class="text-center">Login or Register</p>
+          <p class="text-center">Please use the links above to login or register</p>
         @else
           <p class="text-center">Hello {{ Auth::user()->name }}</p>
         @endif
